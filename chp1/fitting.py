8a5e7e508016dad3be879a6649931ea127607f66
Yurui Ming <yuming@cibci5.eng.uts.edu.au>
2021-01-13 00:11:32 +1100

    modify the grond truth function to polynomial function for better illustration

--- chp1/fitting.py
+++ chp1/fitting.py
@@ -9,14 +9,34 @@ import pdb
 
 utils.GPUConfig()(gpu = 1)
 
+
+class PolynomialFn(snt.Module):
+    def __init__(self, name = "polynomial_fn"):
+        super(PolynomialFn, self).__init__(name = name)
+
+    @snt.once
+    def _initialize(self):
+        self._coeffs = [tf.constant(c, dtype = tf.float32) for c in [-6.4, 6.4, 1.6]]
+        self._ini = tf.constant([0.0], dtype = tf.float32)
+
+    def __call__(self, x):
+        self._initialize()
+        y = tf.scan(lambda unused, v: tf.math.polyval(self._coeffs, v), x, self._ini)
+
+        return y
+    
+
+true_fun = PolynomialFn() # lambda X: tf.math.cos(1.5 * np.pi * X)
+
 class GenSample(snt.Module):
-    def __init__(self, n_samples = 32, name = "gen_sample"):
+    def __init__(self, n_samples = 8, name = "gen_sample"):
         super(GenSample, self).__init__(name = name)
         self._n_samples = n_samples
-        self._true_fun = lambda X: tf.math.cos(1.5 * np.pi * X)
+        # self._true_fun = lambda X: tf.math.cos(1.5 * np.pi * X)
+        self._true_fun = true_fun
 
     def __call__(self):
-        X = tf.sort(tf.random.uniform([self._n_samples, 1]))
+        X = tf.sort(tf.linspace([0.0], [1.0], 8, axis = 0))
         y = self._true_fun(X) + tf.random.normal([self._n_samples, 1]) * 0.1
 
         return (X, y)
@@ -28,15 +48,15 @@ class GenSample(snt.Module):
 log = utils.Log('output')
 X, y = GenSample()()
 
-degrees = [1, 4, 15]
+degrees = [1, 2, 10]
 
-num_epochs = 10000
-opt = snt.optimizers.SGD(learning_rate = 0.05)
+num_epochs = 100000
+opt = snt.optimizers.SGD(learning_rate = 0.2)
 
 X_test = tf.linspace([0.0], [1.0], 100, axis = 0)
 fig = plt.figure(figsize= (12.8, 4.8))
 
-pt = 10
+pt = 11
 plt.rc('font', size=pt)          # controls default text sizes
 plt.rc('axes', titlesize=pt)     # fontsize of the axes title
 plt.rc('axes', labelsize=pt)    # fontsize of the x and y labels
@@ -44,7 +64,7 @@ plt.rc('xtick', labelsize=pt)    # fontsize of the tick labels
 plt.rc('ytick', labelsize=pt)    # fontsize of the tick labels
 plt.rc('legend', fontsize=pt)    # legend fontsize
 
-true_fun = lambda X: tf.math.cos(1.5 * np.pi * X)
+# true_fun = lambda X: tf.math.cos(1.5 * np.pi * X)
 
 for i in range(len(degrees)):
     model = utils.PolynomialRegression(degree=degrees[i])
@@ -56,6 +76,7 @@ for i in range(len(degrees)):
             logits = model(X)
             loss = utils.LossRegression()(logits, y)
 
+            # print("loss: {}".format(loss))  
         params = model.trainable_variables
         grads = tape.gradient(loss, params)
         opt.apply(grads, params)
@@ -70,8 +91,8 @@ for i in range(len(degrees)):
     plt.plot(X_test, y_poly_pred, label="Model")
     plt.plot(X_test, true_fun(X_test), label="True function")
     plt.scatter(X, y, edgecolor='b', s=20, label="Samples")
-    plt.xlim((0, 1))
-    plt.ylim((-2, 2))
+    plt.xlim((0.0, 1.0))
+    plt.ylim((0.0, 4.0))
     plt.legend(loc="best")
 
 fig.tight_layout()
